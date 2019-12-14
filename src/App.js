4fdd2ad53fd9a1be68d92af7c79f682c0e325a27
bembengggg <beng.1411050084@mail.darmajaya.ac.id>
2019-12-15 00:02:27 +0700

    reload

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Hello world
+          Edit <code>src/App.js</code> reload
         </p>
         <a
           className="App-link"
